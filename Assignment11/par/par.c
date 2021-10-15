17ed2ecdb2bded5d6ecda738f8e337f48977cc5e
itays123 <itays2005@gmail.com>
2021-10-15 07:42:30 -0400

    INIT: Par.c question

---
+++ Assignment11/par/par.c
@@ -0,0 +1,11 @@
+/* 
+* Program will validate a given c code sequence of parenthesis.
+*/
+#include <stdio.h>
+
+#define maxRowLen 100
+#define false 0
+#define true 1
+
+/* Prints a formatted message to the user */
+void notifyLineBalanced(int , int );
