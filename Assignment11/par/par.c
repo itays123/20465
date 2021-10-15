f7cc7f06a6a1ea3c404aa4f2362616e3466ee370
itays123 <itays2005@gmail.com>
2021-10-15 07:57:50 -0400

    Init: header of par.c

--- Assignment11/par/par.c
+++ Assignment11/par/par.c
@@ -6,6 +6,30 @@
 #define maxRowLen 100
 #define false 0
 #define true 1
+#define commentMarker #
+#define rowEndMarker 101
+
+/* Add a char to a row in a specified length.
+* Ignores white chars.
+* returns the new row length. If line ends, will return 101 (max row length is 100) */
+int addCharToRow(char, char[], int);
 
 /* Prints a formatted message to the user */
-void notifyLineBalanced(int , int );
+void notifyLineBalanced(int , char [], int );
+
+/* Prints a formatted message to the user */
+void notifyTextBalance(int );
+
+/* Checks if a line has a single { or } */
+int checkSingleBraket(char []);
+
+/* Checks if a line is balanced. 
+* Handle differently if line starts with a comment */
+int checkLineBalance(char [], int );
+
+int main() {
+    char currentRow[maxRowLen];
+    int currentRowLength;
+
+    return 0;
+}
