8e70e0d0848e537e561977014e5dd7a71b6e84a2
itays123 <itays2005@gmail.com>
2021-11-09 01:47:13 -0500

    REFACTOR: Change complex.h prototypes

--- Assignment22/mycomp/complex.h
+++ Assignment22/mycomp/complex.h
@@ -5,20 +5,20 @@ typedef struct complex {
     double real, imaginary;
 } complex;
 
-complex new_comp();
+complex* new_comp();
 
-complex read_comp(double, double);
+void read_comp(complex*, double, double);
 
-void print_comp(complex );
+void print_comp(complex* );
 
-void add_comp(complex, complex);
+void add_comp(complex*, complex*);
 
-void subtract_comp(complex, complex);
+void subtract_comp(complex*, complex*);
 
-void mult_comp_real(complex, double);
+void mult_comp_real(complex*, double);
 
-void mult_comp_img(complex, double);
+void mult_comp_img(complex*, double);
 
-void mult_comp_comp(complex, complex);
+void mult_comp_comp(complex*, complex*);
 
-void abs_comp(complex);
+void abs_comp(complex*);
--- Assignment22/mycomp/mycomp.c
+++ Assignment22/mycomp/mycomp.c
@@ -19,47 +19,47 @@ int main() {
                     && arg_validate(&lineptr, VARIABLE, &chararg1)
                     && arg_validate(&lineptr, NUMBER, &numarg1)
                     && arg_validate(&lineptr, NUMBER, &numarg2))
-                    *(get_variable(chararg1)) = read_comp(numarg1, numarg2);
+                    read_comp(get_variable(chararg1), numarg1, numarg2);
                 break;
             case PRINT:
                 if (arg_count_validate(line, 1) 
                     && arg_validate(&lineptr, VARIABLE, &chararg1))
-                    print_comp(*(get_variable(chararg1)));
+                    print_comp(get_variable(chararg1));
                 break;
             case ADD_COMP:
                 if (arg_count_validate(line, 2) 
                     && arg_validate(&lineptr, VARIABLE, &chararg1)
                     && arg_validate(&lineptr, VARIABLE, &chararg2))
-                    add_comp(*(get_variable(chararg1)), *(get_variable(chararg2)));
+                    add_comp(get_variable(chararg1), get_variable(chararg2));
                 break;
             case SUB_COMP:
                 if (arg_count_validate(line, 2) 
                     && arg_validate(&lineptr, VARIABLE, &chararg1)
                     && arg_validate(&lineptr, VARIABLE, &chararg2))
-                    subtract_comp(*(get_variable(chararg1)), *(get_variable(chararg2)));
+                    subtract_comp(get_variable(chararg1), get_variable(chararg2));
                 break;
             case MULT_COMP_REAL:
                 if (arg_count_validate(line, 2) 
                     && arg_validate(&lineptr, VARIABLE, &chararg1)
                     && arg_validate(&lineptr, NUMBER, &numarg1))
-                    mult_comp_real(*(get_variable(chararg1)), numarg1);
+                    mult_comp_real(get_variable(chararg1), numarg1);
                 break;
             case MULT_COMP_IMG:
                 if (arg_count_validate(line, 2) 
                     && arg_validate(&lineptr, VARIABLE, &chararg1)
                     && arg_validate(&lineptr, NUMBER, &numarg1))
-                    mult_comp_img(*(get_variable(chararg1)), numarg1);
+                    mult_comp_img(get_variable(chararg1), numarg1);
                 break;
             case MULT_COMP_COMP:
                 if (arg_count_validate(line, 2) 
                     && arg_validate(&lineptr, VARIABLE, &chararg1)
                     && arg_validate(&lineptr, VARIABLE, &chararg2))
-                    mult_comp_comp(*(get_variable(chararg1)), *(get_variable(chararg2)));
+                    mult_comp_comp(get_variable(chararg1), get_variable(chararg2));
                 break;
             case ABS_COMP:
                 if (arg_count_validate(line, 1) 
                     && arg_validate(&lineptr, VARIABLE, &chararg1))
-                    abs_comp(*(get_variable(chararg1)));
+                    abs_comp(get_variable(chararg1));
                 break;
             default: /* Undefined command name or error accured */
                 break;
