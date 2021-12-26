f8fcc37596c64ee4d65ceb19998486b7b5f65368
itays123 <itays2005@gmail.com>
2021-12-26 10:37:36 -0500

    FEATURE: Partial string utils impl

--- Assignment14/assembler/charsequence.c
+++ Assignment14/assembler/charsequence.c
@@ -1,6 +1,7 @@
 /* Charsequence.c - working with strings and pointers */
-#include <ctype.h>
-#include "globals.h" /* For input_status */
+#include <ctype.h> /* For isalnum, isdigit */
+#include <string.h> /* For strcmp */
+#include <stdlib.h> /* For atoi */
 #include "charsequence.h"
 
 /******** NEXT FUNCTIONS DEFINITIONS */
@@ -31,4 +32,51 @@ char *next(char target, char *str)
     char *p;
     for (p = str; *p && *p != target; p++);
     return p;
+}
+
+/********* STRING UTILITIES FUNCTIONS ****************/
+
+boolean str_equal(char *start, char *end, char *compare_to)
+{
+    int result;
+    char temp = *end;
+    *end = '\0';
+    result = strcmp(start, compare_to);
+    *end = temp;
+    return result ? TRUE : FALSE;
+}
+
+input_status str_to_int(char *start, char *end, int *to_assign)
+{
+    char temp = *end;
+    
+    /* Validate numeric string */
+    if ((!isdigit(*start) && *start != '-' && *start != '+') /* start can point to a sign */
+        || next_non_digital(start + 1) != end) /* rest of chars in substring must be digits */
+        return INVALID_OPERAND_NOT_NUMBER;
+    
+    *end = '\0';
+    *to_assign = atoi(start);
+    *end = temp;
+    return PASS;
+}
+
+boolean find_opword(char *label_start_maybe, char *label_end_maybe,
+                char **opstart, char **opend)
+{
+    if (*label_end_maybe == ':')
+    {
+        *opstart = next_nonwhite(label_end_maybe + 1);
+        *opend = next_white(opstart);
+        return TRUE;
+    }
+    
+    *opstart = label_start_maybe;
+    *opend = label_end_maybe;
+    return FALSE;
+}
+
+input_status find_operand(char **str, char **start, char **end, input_status not_found_status)
+{
+    
 }
