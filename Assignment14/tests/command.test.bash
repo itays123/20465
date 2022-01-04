f6c6dc7e0c42a8d8ce0faed4f70c7d7e0f423822
itays123 <itays2005@gmail.com>
2022-01-04 02:28:45 -0500

    FIX: Command test noted

--- Assignment14/tests/command.test.bash
+++ Assignment14/tests/command.test.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "$(gcc -g ../assembler/command.c ../assembler/charsequence.c command.c -Wall -pedantic -std=c90 -o command)"
-echo "$(./command)"
+echo "$(./command <command.input.txt >command.out.txt)"
