b3cdeff33162dc61df6d84145b286df0721a0bbe
itays123 <itays2005@gmail.com>
2022-01-04 02:33:34 -0500

    TEST: Create output files for all tests

--- Assignment14/tests/charseq.test.bash
+++ Assignment14/tests/charseq.test.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "$(gcc -g ../assembler/charsequence.c charsequence.c -Wall -pedantic -std=c90 -o charsequence)"
-echo "$(./charsequence)"
+echo "$(./charsequence >charseq.out.txt)"
+echo "$(cat ./charseq.out.txt)"
--- Assignment14/tests/word.test.bash
+++ Assignment14/tests/word.test.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "$(gcc -g ../assembler/word.c ../assembler/utils.c word.c -Wall -pedantic -std=c90 -o word)"
-echo "$(./word)"
+echo "$(./word >word.out.txt)"
+echo "$(cat ./word.out.txt)"
